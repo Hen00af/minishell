4eb6dc8ac670a67b4e42fe1629d3936ca5e2ae0f
Seiya Hattori <shattori@c4r2s7.42tokyo.jp>
2025-05-15 17:04:45 +0900

    a

--- main.c
+++ main.c
@@ -1,16 +1,4 @@
 #include "minishell.h"
-#include <ctype.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <readline/history.h>
-#include <readline/readline.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
 
 static void	prompt(void);
 
