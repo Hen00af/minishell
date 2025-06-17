edf99b36d2368bacab183533d9b89e7c816ccd79
Seiya Hattori <shattori@c4r3s6.42tokyo.jp>
2025-06-17 16:04:21 +0900

    complete linerlizer exept for norm, file construct

--- linerlizer/linerlizer.c
+++ linerlizer/linerlizer.c
@@ -6,11 +6,11 @@
 /*   By: shattori <shattori@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 16:14:37 by shattori          #+#    #+#             */
-/*   Updated: 2025/06/15 18:33:05 by shattori         ###   ########.fr       */
+/*   Updated: 2025/06/17 16:02:23 by shattori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./linerlizer.h"
+#include "linerlizer.h"
 
 // ====== AND/OR ======
 t_andor	*linearize_andor(t_ast *ast)
@@ -25,23 +25,22 @@ t_andor	*linearize_andor(t_ast *ast)
 	node->right = linearizer(ast->right);
 	return (node);
 }
+
 t_redir_type	map_redir_type(t_node_type type)
 {
 	if (type == NODE_REDIR_IN)
 		return (REDIR_IN);
-	else if (type == NODE_REDIR_OUT)
+	if (type == NODE_REDIR_OUT)
 		return (REDIR_OUT);
-	else if (type == NODE_REDIR_APPEND)
+	if (type == NODE_REDIR_APPEND)
 		return (REDIR_APPEND);
-	else if (type == NODE_HEREDOC)
+	if (type == NODE_HEREDOC)
 		return (REDIR_HEREDOC);
-	else
-	{
-		ft_fprintf(2, "Unknown redirection type in map_redir_type\n");
-		exit(1);
-	}
+	fprintf(stderr, "Unknown redirection type!\n");
+	exit(1);
 }
-// ====== SIMPLE_COMMAND (2段階化) ======
+
+// ====== SIMPLE COMMAND ======
 t_command	*linearize_simple_command_to_command(t_ast *ast)
 {
 	t_command		*cmd;
@@ -55,7 +54,6 @@ t_command	*linearize_simple_command_to_command(t_ast *ast)
 	cmd->argv = ast->argv;
 	cmd->redirections = NULL;
 	cmd->subshell_ast = NULL;
-	// Redirection の再帰処理 (flattenする)
 	if (ast->type == NODE_REDIR_IN || ast->type == NODE_REDIR_OUT
 		|| ast->type == NODE_REDIR_APPEND || ast->type == NODE_HEREDOC)
 	{
@@ -65,11 +63,12 @@ t_command	*linearize_simple_command_to_command(t_ast *ast)
 		redir->filename = ast->filename;
 		redir_node = ft_lstnew(redir);
 		ft_lstadd_back(&child->redirections, redir_node);
-		free(cmd); // いま作ったcmdは不要
+		free(cmd);
 		return (child);
 	}
 	return (cmd);
 }
+
 t_andor	*linearize_simple_command(t_ast *ast)
 {
 	t_command	*cmd;
@@ -82,10 +81,9 @@ t_andor	*linearize_simple_command(t_ast *ast)
 	andor = malloc(sizeof(t_andor));
 	andor->type = ANDOR_PIPELINE;
 	andor->pipeline = pipeline;
-	andor->left = NULL;
-	andor->right = NULL;
 	return (andor);
 }
+
 // ====== PIPELINE ======
 void	flatten_pipeline(t_ast *node, t_pipeline *pipeline)
 {
@@ -104,6 +102,7 @@ void	flatten_pipeline(t_ast *node, t_pipeline *pipeline)
 		ft_lstadd_back(&pipeline->commands, new_node);
 	}
 }
+
 t_andor	*linearize_pipeline(t_ast *ast)
 {
 	t_pipeline	*pipeline;
@@ -115,10 +114,9 @@ t_andor	*linearize_pipeline(t_ast *ast)
 	node = malloc(sizeof(t_andor));
 	node->type = ANDOR_PIPELINE;
 	node->pipeline = pipeline;
-	node->left = NULL;
-	node->right = NULL;
 	return (node);
 }
+
 // ====== SUBSHELL ======
 t_andor	*linearize_subshell(t_ast *ast)
 {
@@ -127,19 +125,18 @@ t_andor	*linearize_subshell(t_ast *ast)
 	t_andor		*andor;
 
 	cmd = malloc(sizeof(t_command));
-	pipeline = malloc(sizeof(t_pipeline));
-	andor = malloc(sizeof(t_andor));
-	cmd->subshell_ast = linearizer(ast->left);
 	cmd->argv = NULL;
 	cmd->redirections = NULL;
+	cmd->subshell_ast = linearizer(ast->left);
+	pipeline = malloc(sizeof(t_pipeline));
 	pipeline->commands = ft_lstnew(cmd);
+	andor = malloc(sizeof(t_andor));
 	andor->type = ANDOR_PIPELINE;
 	andor->pipeline = pipeline;
-	andor->left = NULL;
-	andor->right = NULL;
 	return (andor);
 }
-// ====== MAIN DISPATCH ======
+
+// ====== DISPATCH ======
 t_andor	*linearizer(t_ast *ast)
 {
 	if (!ast)
@@ -152,7 +149,60 @@ t_andor	*linearizer(t_ast *ast)
 		return (linearize_subshell(ast));
 	return (linearize_simple_command(ast));
 }
-// ====== 簡易テスト用PRINT ======
+
+// ====== PRINT TEST ======
+void	print_redirections(t_list *redir_list, int indent)
+{
+	t_redirection	*redir;
+
+	while (redir_list)
+	{
+		redir = (t_redirection *)redir_list->content;
+		for (int i = 0; i < indent; i++)
+			printf("  ");
+		printf("REDIR: ");
+		if (redir->type == REDIR_IN)
+			printf("< ");
+		else if (redir->type == REDIR_OUT)
+			printf("> ");
+		else if (redir->type == REDIR_APPEND)
+			printf(">> ");
+		else if (redir->type == REDIR_HEREDOC)
+			printf("<< ");
+		printf("%s\n", redir->filename);
+		redir_list = redir_list->next;
+	}
+}
+
+void	print_commands(t_pipeline *pipeline, int indent)
+{
+	t_list		*cmd_list;
+	t_command	*cmd;
+
+	cmd_list = pipeline->commands;
+	while (cmd_list)
+	{
+		cmd = (t_command *)cmd_list->content;
+		for (int i = 0; i < indent; i++)
+			printf("  ");
+		if (cmd->argv)
+		{
+			printf("COMMAND:");
+			for (int i = 0; cmd->argv[i]; i++)
+				printf(" %s", cmd->argv[i]);
+			printf("\n");
+		}
+		else
+		{
+			printf("SUBSHELL:\n");
+			print_linerlized_ast(cmd->subshell_ast, indent + 1);
+		}
+		if (cmd->redirections)
+			print_redirections(cmd->redirections, indent + 1);
+		cmd_list = cmd_list->next;
+	}
+}
+
 void	print_linerlized_ast(t_andor *tree, int indent)
 {
 	if (!tree)
@@ -160,14 +210,28 @@ void	print_linerlized_ast(t_andor *tree, int indent)
 	for (int i = 0; i < indent; i++)
 		printf("  ");
 	if (tree->type == ANDOR_AND)
+	{
 		printf("AND\n");
+		if (tree->left)
+			print_linerlized_ast(tree->left, indent + 1);
+		if (tree->right)
+			print_linerlized_ast(tree->right, indent + 1);
+	}
 	else if (tree->type == ANDOR_OR)
+	{
 		printf("OR\n");
+		if (tree->left)
+			print_linerlized_ast(tree->left, indent + 1);
+		if (tree->right)
+			print_linerlized_ast(tree->right, indent + 1);
+	}
 	else if (tree->type == ANDOR_PIPELINE)
+	{
 		printf("PIPELINE\n");
-	print_linerlized_ast(tree->left, indent + 1);
-	print_linerlized_ast(tree->right, indent + 1);
+		print_commands(tree->pipeline, indent + 1);
+	}
 }
+
 // ====== MAIN ======
 int	main(int ac, char **av)
 {
@@ -179,6 +243,7 @@ int	main(int ac, char **av)
 		return (0);
 	lex = lexer(av[1]);
 	ast = start_parse(lex);
+	// print_ast(ast, 0);
 	linearized_ast = linearizer(ast);
 	print_linerlized_ast(linearized_ast, 0);
 	return (0);
