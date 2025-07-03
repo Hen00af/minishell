c21780dbc111efc95a3e615da14561a5cd040455
Naruki Ando <nando@c1r4s3.42tokyo.jp>
2025-07-03 21:03:33 +0900

    heredoc実行可能

--- linearizer/linearizer.c
+++ linearizer/linearizer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   linearizer.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shattori <shattori@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nando <nando@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 16:14:37 by shattori          #+#    #+#             */
-/*   Updated: 2025/07/03 20:26:37 by shattori         ###   ########.fr       */
+/*   Updated: 2025/07/03 21:00:24 by nando            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,9 +65,9 @@ t_command	*linearize_simple_command_to_command(t_ast *ast, t_shell *shell)
 		redir->type = map_redir_type(ast->type);
 		redir_node = ft_lstnew(redir);
 		ft_lstadd_back(&child->redirections, redir_node);
+		redir->filename = ast->filename;
 		if (ast->type == NODE_HEREDOC)
 			redir->filename = process_heredoc(redir_node, shell);
-		redir->filename = ast->filename;
 		free(cmd);
 		return (child);
 	}
