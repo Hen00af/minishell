be1738e31e412dbb1c6c879c7f9655595b7b3c47
Seiya Hattori <shattori@c1r3s6.42tokyo.jp>
2025-07-24 17:45:32 +0900

    WIP: 一時的な作業保存

--- executor/exec_fank.c
+++ executor/exec_fank.c
@@ -6,52 +6,11 @@
 /*   By: shattori <shattori@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/21 14:24:41 by shattori          #+#    #+#             */
-/*   Updated: 2025/07/24 17:33:25 by shattori         ###   ########.fr       */
+/*   Updated: 2025/07/24 17:44:58 by shattori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
-
-static void	exec_child_process(t_exec *exec, t_command *cmd, t_shell *shell,
-		int has_next)
-{
-	if (exec->prev_fd != -1)
-		dup2(exec->prev_fd, STDIN_FILENO);
-	if (has_next)
-		dup2(exec->pipefd[1], STDOUT_FILENO);
-	if (exec->prev_fd != -1)
-		close(exec->prev_fd);
-	if (has_next)
-	{
-		close(exec->pipefd[0]);
-		close(exec->pipefd[1]);
-	}
-	handle_redirections(cmd);
-	if (cmd->subshell_ast)
-		exit(exec_subshell(cmd, shell));
-	else if (is_builtin(cmd->argv[0]))
-		exit(exec_builtin(cmd->argv, shell->env));
-	exit(exec_simple_command(cmd, shell));
-}
-
-static int	handle_child_and_parent(t_exec *exec, t_command *cmd,
-		t_shell *shell, int has_next)
-{
-	exec->pid = fork();
-	if (exec->pid == -1)
-		return (perror("fork"), 1);
-	if (exec->pid == 0)
-		exec_child_process(exec, cmd, shell, has_next);
-	if (exec->prev_fd != -1)
-		close(exec->prev_fd);
-	if (has_next)
-		close(exec->pipefd[1]);
-	if (has_next)
-		exec->prev_fd = exec->pipefd[0];
-	else
-		exec->prev_fd = -1;
-	return (0);
-}
 
 static int	exec_pipeline_loop(t_list *cmd_list, t_exec *exec, t_shell *shell)
 {
