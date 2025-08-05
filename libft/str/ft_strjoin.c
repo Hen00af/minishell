4f973f8cef42eb8882a41eda5b87f63e5c8af0ac
Seiya Hattori <shattori@c4r2s7.42tokyo.jp>
2025-08-05 15:42:54 +0900

    ok

--- libft/str/ft_strjoin.c
+++ libft/str/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: shattori <shattori@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 17:05:57 by shattori          #+#    #+#             */
-/*   Updated: 2024/10/26 19:56:09 by shattori         ###   ########.fr       */
+/*   Updated: 2025/08/05 15:34:12 by shattori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*str;
 	size_t	total_len;
 
+	if (!s1 && !s2)
+		return (NULL);
 	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
 	str = malloc(total_len * sizeof(char));
 	if (!str)
